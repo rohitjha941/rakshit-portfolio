50e73b8f3c4b01e6d57d7e7b79677dd5f9023993
Rohit <rjha@ph.iitr.ac.in>
2019-08-24 00:12:08 +0530

    Updates

---
+++ precache-manifest.7c0bbda830a6d3159ec28c22214474ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96813575da8fd376a80bd5fd0ddc51b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394016ee0ba614a76bd0",
+    "url": "/static/css/main.63810c12.chunk.css"
+  },
+  {
+    "revision": "18fe0c003f9ba6747ca2",
+    "url": "/static/js/2.5a629fe2.chunk.js"
+  },
+  {
+    "revision": "394016ee0ba614a76bd0",
+    "url": "/static/js/main.54de0a10.chunk.js"
+  },
+  {
+    "revision": "82692455fbdfbde2001a",
+    "url": "/static/js/runtime~main.5cb4dc75.js"
+  },
+  {
+    "revision": "30ae297323478787d1de6606895cf9d1",
+    "url": "/static/media/background.30ae2973.png"
+  },
+  {
+    "revision": "eacf5e5033f300aed1fb0e5b71ec57e6",
+    "url": "/static/media/home-illustration.eacf5e50.png"
+  }
+]);
