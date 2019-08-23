6fca94f9ee2d68f3a3ab90833aa9aa0095b5a065
Rohit <rjha@ph.iitr.ac.in>
2019-08-24 03:05:39 +0530

    Added Social Links

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,6 +4,7 @@ import FaceBook from "../../static/Icons/facebook.png";
 import Dribbble from "../../static/Icons/dribbble.png";
 import Behance from "../../static/Icons/behance.png";
 import Linkedin from "../../static/Icons/linkedin.png";
+import Medium from "../../static/Icons/Path.png";
 
 
 
@@ -29,6 +30,28 @@ export default class Contact extends Component {
 
                     <div style = {{"marginTop" : "2rem"}} className = "contact-title">
                     Find me on
+                    </div>
+
+                    <div className  = "conact-array">
+                        <a href = "https://medium.com/@rakshitkeswani13" target = "blank"> 
+                            <img src = {Medium} alt  = "Medium" className = "icons" /> 
+                        </a>
+
+                        <a href = "https://www.linkedin.com/in/rakshit-keswani" target = "blank"> 
+                            <img src = {Linkedin} alt  = "Linkedin" className = "icons" /> 
+                        </a>
+
+                        <a href = "https://www.facebook.com/rakshit.keswani" target = "blank"> 
+                            <img src = {FaceBook} alt  = "Facebook" className = "icons" /> 
+                        </a>
+
+                        <a href = "https://dribbble.com/RakshitK" target = "blank"> 
+                            <img src = {Dribbble} alt  = "Dribbble" className = "icons" /> 
+                        </a>
+
+                        <a href = "https://www.behance.net/rakshitkes3b15" target = "blank"> 
+                            <img src = {Behance} alt  = "Behance" className = "icons" /> 
+                        </a>
                     </div>
                 </div>
 
